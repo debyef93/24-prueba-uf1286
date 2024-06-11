af9b184105a8f93962846b07b3cbbd965a70f652
deby <escuderoflores1993@gmail.com>
2024-06-11 09:54:54 +0200

    usamos el Math

--- main.js
+++ main.js
@@ -3,43 +3,20 @@ function generarArrayBidimensional() {
     return [0, 1, 2,];
   }
 
-  var array = [1,4,6,10];
+  var array = [1[3], 4[2], 6[1], 10[0]];
   for (var i = 0; i < 10; i++) {
     array.push(generarArray());
   }
   return array;
 }
 
- var array1= [2,5,7,9];
-  for (var i=0; < 10; i++){
-    array1.push(generarArray());
-  }
-   return array;
-
-
-   
-
-function mostrarArrayBidimensional(arrayBidimensional) {
-  function mostrarArray(array) {
-    array.forEach(function (elemento) {
-      var formatedElemento = (" " + elemento).slice(-2);
-      process.stdout.write(`| ${formatedElemento} `);
-    });
-    console.log("|");
-  }
-
-  function mostrarLinea() {
-    for (var i = 0; i < 15; i++) {
-      process.stdout.write("-");
-    }
-    console.log("-");
-  }
-
-  mostrarLinea();
-  arrayBidimensional.forEach(function (array) {
-    mostrarArray(array);
-    mostrarLinea();
-  });
+var array1 = [2[7], 5[6], 7[5], 9[4]];
+for (var i = 0; j=10; i++) {
+  array1.push(generarArray());
 }
+return array;
 
-mostrarArrayBidimensional(generarArrayBidimensional());
+var lista=rgb(+Math.floor(),Math.random()*256))+(math.floor(0), Math.random(256)(`,`))=(math.random());
+
+console.log(array);
+console.log(array1);
